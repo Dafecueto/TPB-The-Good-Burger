7196bbf617636dc068921dcecc290fcc7320407b
Dafecueto <dav0star1991@gmail.com>
2020-11-17 13:12:54 +0100

    refactor(index):

--- src/services/working.js
+++ src/services/working.js
@@ -1,16 +1,22 @@
-const order = 
-[
-    {
-        name: "Burguer Simple",
-        price: 4
-},
-    {
-        name: "Chicken Burger",
-        price: 4
-    }
-]
+const order = [
+  {
+    name: 'Burger Simple',
+    price: 4,
+  },
+  {
+    name: 'Chicken Burger',
+    price: 4,
+  },
+];
 
-const totalprice = order.reduce(
+/* const totalprice = order.reduce(
     (totalprice, {price}) =>  totalprice += price, 0);
 
-    console.log(totalprice)
+    console.log(totalprice) */
+
+const name = 'Burger Simple';
+
+const selectedProduct = order.find(product => product.name === name);
+console.log(selectedProduct);
+selectedProduct.price = selectedProduct.price + 1;
+console.log(selectedProduct);
--- src/views/menu.view.js
+++ src/views/menu.view.js
@@ -1,13 +1,66 @@
 class MenuView {
     constructor () {
-        $images = document.getElementById("images-menu");
-        $tableOrder = document.getElementById("order-tbody");
+        this.$images = document.getElementById("images-menu");
+        this.$tableOrder = document.getElementById("order-tbody");
+        this.order = [
+            {
+                name: "Burger Simple",
+                price : 4,
+                quantity = 0
+            },
+            {
+                name: "Burger Doble",
+                price: 5,
+                quantity: 0
+            },
+            {
+                name: "Chicken Burger",
+                price: 4,
+                quantity: 0
+            },
+            {
+                name: "Coca Cola",
+                price: 2,
+                quantity: 0,
+            },
+            {
+                name: "Café",
+                price: 1,
+                quantity: 0,
+            }
+        ]   
     }
 
-    addRowToOrder(elementProduct) {
-        const tableRow = $tableOrder.insertRow(0);
-        tableRow.insertCell(0) = elementProduct.value;
-        tableRow.insertCell(1) = elementProduct.dataset.price;
-        tableRow.insertCell(2) = 1;
+    addRowToOrder(name, price) {
+        const selectedProduct = order.find(product => product.name === name);
+        const htmlOrderTable = order.reduce((table, product) => {
+          if (product.name === selectedProduct.name && product.quantity >= 0 && product.quantity <= 2) {
+            table += `<tr><td>${product.name}</td><td>${product.price}</td>
+                <td>${product.quantity + 1}
+                <td><input type='button' id='remove' name='${name}' value='-' onclick='remove()'></td></tr>`;
+          }
+          return table;
+        }, '');
+        console.log(htmlOrderTable);
+
+        $tableOrder.innerHTML += htmlOrderTable;
+      }
+
+    
+
+    setQuantity(name) {
+        const selectedProduct = this.order.find(product => product.name === name);
+        selectedProduct.quantity = selectedProduct.quantity + 1;
+        
+    }
+
+    bindClickProduct(handler) {
+        $images.addEventListener("click", event => {
+            const name = event.target.value;
+            const price = event.target.dataset.price;
+            addRowToOrder(name, price)
+            console.log(tableToObject());
+            console.log($tableOrder)
+        })
     }
 }
